1a449225b65b2f86277831e0669143739cd9f80c
Elise Huard <elise@mastodonc.com>
2017-06-01 17:12:33 +0100

    remove failing test

--- test/kixi/hecuba/measurements/processor_test.clj
+++
@@ -1,7 +0,0 @@
-(ns kixi.hecuba.measurements.processor-test
-  (:require [clojure.test :refer :all]
-            [kixi.hecuba.measurements.processor :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
