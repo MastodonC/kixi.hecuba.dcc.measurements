347d3910a9e3ceb7fab80fa47607aa059a5de540
Elise Huard <elise@mastodonc.com>
2017-06-01 17:31:18 +0100

    fix test after change

--- test/kixi/hecuba/dcc/measurements/parser_test.clj
+++ test/kixi/hecuba/dcc/measurements/parser_test.clj
@@ -7,7 +7,7 @@
 
 (deftest parser-test
   (testing "parsing expected measurement xml structure"
-    (let [measurement-data (process measurement-string)]
+    (let [measurement-data (:measurements (process measurement-string))]
       (is (== (count measurement-data) 49))
       (is (every? #(number? (bigdec (:value %))) measurement-data))
       (is (every? #(#{"electricityConsumption" "gasConsumption"} (:type %)) measurement-data)))))
